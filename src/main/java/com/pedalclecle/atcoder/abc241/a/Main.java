b0e27084b8ef475143486cedf7221442193ace98
tetsuji.okuda <ttjspace4entry@gmail.com>
2022-03-17 23:30:53 +0900

    Add ABC421 A

---
+++ src/main/java/com/pedalclecle/atcoder/abc241/a/Main.java
@@ -0,0 +1,23 @@
+package com.pedalclecle.atcoder.abc241.a;
+
+import java.util.Scanner;
+
+/**
+ * AtCoder Beginner Contest
+ * @author pedalclecle
+ */
+
+public class Main {
+
+    StringBuilder answer = new StringBuilder();
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.solve(new Scanner(System.in));
+    }
+
+    public void solve(Scanner sc) {
+
+
+    }
+}
